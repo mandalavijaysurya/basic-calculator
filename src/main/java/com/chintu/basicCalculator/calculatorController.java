baac447d448b35f57746c75b4e481fa794f49a75
ManikantaMandala <mandala.manikanta.20cse@bmu.edu.in>
2023-07-26 21:39:50 +0530

    First commit: After creating the project and giving the basic HTML and CSS

---
+++ src/main/java/com/chintu/basicCalculator/calculatorController.java
@@ -0,0 +1,14 @@
+package com.chintu.basicCalculator;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class calculatorController {
+
+    @RequestMapping(value="/startpage", method = RequestMethod.GET)
+    public String startPage(){
+        return "Calculator";
+    }
+}
